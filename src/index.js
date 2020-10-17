73bc210ae508258e0118423f3141fe87577dfccb
VictoriaKacer <kacer1703@gmail.com>
2020-10-17 15:17:49 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const a = [];
+    const brackets = Object.fromEntries(bracketsConfig);
+    console.log(brackets);
+    for (const el of str.split``) {
+        if (a[0] === el) {
+            a.shift();
+        } else if (brackets[el]) {
+            a.unshift(brackets[el]);
+        } else {
+            return false;
+        }
+    }
+
+    return a.length === 0;
+    }
